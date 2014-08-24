5ba8dbf94e66b649e17fc9ae0d80ab9e08e48895
Diego Medina <d.medina1290@gmail.com>
2014-08-24 17:23:30 -0400

    finished sorting feature

--- job-board-master/app/helpers/application_helper.rb
+++ job-board-master/app/helpers/application_helper.rb
@@ -13,7 +13,15 @@ module ApplicationHelper
 
   def sortable(column, title = nil)
     title ||= column.titleize
-    direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    css_class = column == sort_column ? "current #{sort_direction}" :nil
+    #ensure that the jobs table starts with end_date "asc"
+    #and that when clicked sorts "desc" while anything else starts off "asc"
+    if column == "end_date" && params[:sort].nil? && params[:direction].nil?
+      direction = "desc"
+    else
+      direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+    end
+    search = params[:search]
+    link_to title, {:sort => column, :direction => direction, :search => search}, {:class =>  css_class}
   end
 end
